086ac7d45a0cc2692953334c724adba55761e1ec
Pippin404 <Baconpippin@gmail.com>
2024-10-08 18:49:55 -0400

    Started to implement dash. Also proposal.pdf added

--- finalprojectv2/Scripts/player.gd
+++ finalprojectv2/Scripts/player.gd
@@ -1,15 +1,42 @@
 extends CharacterBody2D
+@onready var timer: Timer = $Timer
 
-
+var directionSide=0;
+var dashing=false;
+var storeTempVelocity=0;
 const SPEED = 120.0
 const JUMP_VELOCITY = -250.0
+var DASHMAG=100;
+var XcustomVelocity=0;
 
 
 func _physics_process(delta: float) -> void:
+	# ADD X VELOCITY
+	velocity.x = XcustomVelocity;
 	
-	# Add the gravity.
+	# If grounded, be on floor
+	if is_on_floor():
+		XcustomVelocity=0;
+	
+	# Add the gravity. if dashing, add a little less. 
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		if dashing==false:
+			velocity += get_gravity() * delta
+		else:
+			velocity +=get_gravity()/5 * delta;
+			
+
+
+	#DASH TIME!!!!
+	if Input.is_action_just_pressed("ui_text_caret_left"):
+		dashing=!dashing;
+		timer.start();
+		
+
+	#if dashing==true:
+		
+		
+
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -18,9 +45,21 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
+	if direction and dashing==false:
+		if direction <0:
+			directionSide=1;
+		else:
+			directionSide=0;
+			
 		velocity.x = direction * SPEED
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_timer_timeout() -> void:
+	dashing=false;
+	velocity.y=storeTempVelocity;
+	XcustomVelocity=SPEED/6;
